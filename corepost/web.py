ce58b140e6c719dfd4aa9a693919227e74d52818
czaanja <czaanja@gmail.com>
2015-11-30 18:03:20 +0100

    Convert response to bytes to avoid twisted err.

--- corepost/web.py
+++ corepost/web.py
@@ -19,14 +19,14 @@ from zope.interface import implements
 # CLASSES
 #
 #########################################################
-    
+
 class RESTResource(Resource):
     '''
     Main resource responsible for routing REST requests to the implementing methods
     '''
     isLeaf = True
     implements(IRESTResource)
-    
+
     def __init__(self,services=(),schema=None,filters=()):
         '''
         Constructor
@@ -38,19 +38,19 @@ class RESTResource(Resource):
     def render_GET(self,request):
         """ Handles all GET requests """
         return self.__renderUrl(request)
-    
+
     def render_POST(self,request):
         """ Handles all POST requests"""
         return self.__renderUrl(request)
-    
+
     def render_PUT(self,request):
         """ Handles all PUT requests"""
         return self.__renderUrl(request)
-    
+
     def render_DELETE(self,request):
         """ Handles all DELETE requests"""
         return self.__renderUrl(request)
-    
+
     def __renderUrl(self,request):
         try:
             val = self.__router.getResponse(request)
@@ -67,19 +67,19 @@ class RESTResource(Resource):
         except Exception as ex:
             self.__applyResponse(request, 500, None)
             return str(ex)
-        
+
     def __finishRequest(self,response,request):
         if not request.finished:
             self.__applyResponse(request, response.code,response.headers)
-            request.write(response.entity)
+            request.write(bytes(response.entity))
             request.finish()
-        
+
     def __applyResponse(self,request,code,headers={"content-type":MediaType.TEXT_PLAIN}):
         request.setResponseCode(code)
         if headers != None:
             for header,value in headers.iteritems():
                 request.setHeader(header, value)
-                
+
     def run(self,port=8080):
         """Shortcut for running app within Twisted reactor"""
         factory = Site(self)
@@ -90,7 +90,7 @@ class RESTResource(Resource):
 #
 # DECORATORS
 #
-##################################################################################################    
+##################################################################################################
 
 def route(url,methods=(Http.GET,),accepts=MediaType.WILDCARD,produces=None,cache=True):
     '''
@@ -101,15 +101,15 @@ def route(url,methods=(Http.GET,),accepts=MediaType.WILDCARD,produces=None,cache
             return f
         router = UrlRouter(f, url, methods, accepts, produces, cache)
         setattr(wrap,'corepostRequestRouter',router)
-        
+
         return wrap
     return decorator
-    
+
 def validate(schema=None,**vKwargs):
     '''
     Main decorator for registering additional validators for incoming URL arguments
     '''
-    def fn(realfn):  
+    def fn(realfn):
         def wrap(*args,**kwargs):
             # first run schema validation, then the custom validators
             errors = []
@@ -119,8 +119,8 @@ def validate(schema=None,**vKwargs):
                 except Invalid as ex:
                     for arg, error in ex.error_dict.items():
                         errors.append("%s: %s ('%s')" % (arg,error.msg,error.value))
-             
-            # custom validators    
+
+            # custom validators
             for arg in vKwargs.keys():
                 validator = vKwargs[arg]
                 if arg in kwargs:
@@ -132,11 +132,11 @@ def validate(schema=None,**vKwargs):
                 else:
                     if isinstance(validator,FancyValidator) and validator.not_empty:
                         raise TypeError("Missing mandatory argument '%s'" % arg)
-            
+
             # fire error if anything failed validation
             if len(errors) > 0:
                 raise TypeError('\n'.join(errors))
             # all OK
             return realfn(*args,**kwargs)
         return wrap
-    return fn    
+    return fn
